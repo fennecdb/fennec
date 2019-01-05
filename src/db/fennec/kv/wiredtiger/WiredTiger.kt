690260005bf9300c7dbe9aad8c395dc0d24d6abd
Flakesz <33940069+Flakesz@users.noreply.github.com>
2019-01-05 17:25:20 +0100

    Add static wt lib linkage

--- src/db/fennec/kv/wiredtiger/WiredTiger.kt
+++ src/db/fennec/kv/wiredtiger/WiredTiger.kt
@@ -6,6 +6,9 @@ import com.wiredtiger.db.WiredTigerException
 class WiredTiger {
 
     companion object {
+        init {
+            System.getProperties()["java.library.path"] = "/usr/local/share/java/wiredtiger-3.1.0/"
+        }
 
         @Throws(WiredTigerException::class)
         fun createSession(shouldOpenDirectly: Boolean = false): WiredTigerSession {
