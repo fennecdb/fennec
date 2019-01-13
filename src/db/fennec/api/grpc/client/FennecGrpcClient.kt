d40991e4a253c85e4f3d14bbb2dacd106be8d8be
Flakesz <33940069+Flakesz@users.noreply.github.com>
2019-01-13 14:16:26 +0100

    Add retry logic to the jvm client

--- src/db/fennec/api/grpc/client/FennecGrpcClient.kt
+++ src/db/fennec/api/grpc/client/FennecGrpcClient.kt
@@ -2,6 +2,8 @@ package db.fennec.api.grpc.client
 
 import com.google.common.collect.HashMultimap
 import com.google.common.flogger.FluentLogger
+import com.google.common.util.concurrent.Uninterruptibles
+import db.fennec.api.grpc.client.error.FennecException
 import db.fennec.api.proto.*
 import db.fennec.fql.FQuery
 import db.fennec.proto.FDataEntryProto
@@ -11,6 +13,8 @@ import db.fennec.fql.FResult
 import db.fennec.fql.Key
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
 import java.util.concurrent.TimeUnit
 import java.util.stream.Collectors
 
@@ -36,43 +40,79 @@ internal class FennecGrpcClient(val host: String = "localhost", val port: Int = 
         return FData.fromProto(this.data)
     }
 
-    fun query(query: FQuery): FResult {
-        val result = HashMultimap.create<Key, FData>()
-        for (selection in query.selections) {
-            with (selection) {
-                val resp = stub.deadline()?.query(FQueryReq.newBuilder()
-                        .setField(field)
-                        .setNamespace(ns)
-                        .setInRangeLow(condition.min())
-                        .setInRangeHigh(condition.max())
-                        .build())
-                log.atInfo().log("$resp")
-                if (resp != null) {
-                    for (e: FDataEntryProto in resp.result.dataList) {
-                        val colonIndex = e.field.indexOf(':')
-                        if (colonIndex >= 0) {
-                            result.put(Key(field = e.field.substring(0, colonIndex), ns = ns), e.toNonProto())
+    fun query(query: FQuery): FResult? {
+        return handle {
+            val result = HashMultimap.create<Key, FData>()
+            for (selection in query.selections) {
+                with (selection) {
+                    val resp = stub.deadline()?.query(FQueryReq.newBuilder()
+                            .setField(field)
+                            .setNamespace(ns)
+                            .setInRangeLow(condition.min())
+                            .setInRangeHigh(condition.max())
+                            .build())
+                    log.atInfo().log("$resp")
+                    if (resp != null) {
+                        for (e: FDataEntryProto in resp.result.dataList) {
+                            val colonIndex = e.field.indexOf(':')
+                            if (colonIndex >= 0) {
+                                result.put(Key(field = e.field.substring(0, colonIndex), ns = ns), e.toNonProto())
 
+                            }
                         }
                     }
                 }
             }
+            FResult(result)
         }
-        return FResult(result)
     }
 
     private fun FennecServiceGrpc.FennecServiceBlockingStub?.deadline(): FennecServiceGrpc.FennecServiceBlockingStub? {
         return this?.withDeadlineAfter(DEADLINE_SEC, TimeUnit.SECONDS)
     }
 
+    private fun <T> handle(body: () -> T): T? {
+        var numAttempt = 0
+        var retry = true
+        var lastMessage = ""
+        var result: T? = null
+        while (retry) {
+            try {
+                retry = false
+                numAttempt++
+                result = body.invoke()
+            } catch (e: StatusRuntimeException) {
+                when (e.status) {
+                    Status.UNAVAILABLE -> {
+                        retry = true
+                        log.atWarning().log("Encountered '${e.message}' will retry (attempt $numAttempt)...")
+                        Uninterruptibles.sleepUninterruptibly(500, TimeUnit.MILLISECONDS)
+                    }
+                    else -> {
+                        log.atWarning().withCause(e).log("Action failed due to $e")
+                        if (e.message != null) {
+                            lastMessage = e.message!!
+                        }
+                        throw FennecException(-1, lastMessage)
+                    }
+                }
+            }
+        }
+        return result
+    }
+
     fun insert(data: Iterable<FData>, field: String, ns: String) {
-        val resp = stub.deadline()?.insert(buildWriteReq(data, field, ns))
-        log.atInfo().log("Insert:$resp")
+        handle {
+            val resp = stub.deadline()?.insert(buildWriteReq(data, field, ns))
+            log.atInfo().log("Insert:$resp")
+        }
     }
 
     fun upsert(data: Iterable<FData>, field: String, ns: String) {
-        val resp = stub.deadline()?.upsert(buildWriteReq(data, field, ns))
-        log.atInfo().log("Upsert:$resp")
+        handle {
+            val resp = stub.deadline()?.upsert(buildWriteReq(data, field, ns))
+            log.atInfo().log("Upsert:$resp")
+        }
     }
 
     private fun buildWriteReq(data: Iterable<FData>, field: String, ns: String): FWriteReq {
@@ -89,18 +129,22 @@ internal class FennecGrpcClient(val host: String = "localhost", val port: Int = 
     }
 
     fun remove(lowRange: Long, highRange: Long, field: String, ns: String) {
-        val resp = stub.deadline()?.remove(FRemoveReq.newBuilder()
-                .setField(field)
-                .setNamespace(ns)
-                .setInRangeLow(lowRange)
-                .setInRangeHigh(highRange)
-                .build())
-        log.atInfo().log("Remove:$resp")
+        handle {
+            val resp = stub.deadline()?.remove(FRemoveReq.newBuilder()
+                    .setField(field)
+                    .setNamespace(ns)
+                    .setInRangeLow(lowRange)
+                    .setInRangeHigh(highRange)
+                    .build())
+            log.atInfo().log("Remove:$resp")
+        }
     }
 
     fun removeNamespace(ns: String) {
-        val resp = stub.deadline()?.removeNS(FRemoveNSReq.newBuilder().setNamespace(ns).build())
-        log.atInfo().log("RemoveNS:$resp")
+        handle {
+            val resp = stub.deadline()?.removeNS(FRemoveNSReq.newBuilder().setNamespace(ns).build())
+            log.atInfo().log("RemoveNS:$resp")
+        }
     }
 
     fun close() {
@@ -112,60 +156,4 @@ internal class FennecGrpcClient(val host: String = "localhost", val port: Int = 
 
         private val DEADLINE_SEC: Long = 15
     }
-}
-
-fun main(args: Array<String>) {
-//    val log = FluentLogger.forEnclosingClass()
-//    val ns = "fennec_client_test"
-////    val rateLimiter = RateLimiter.create(1.0)
-////
-//////    while (true) {
-////        for (i in 0..1000) {
-////            val sin = Math.sin(0.01 * i)
-////            println("sin:$sin")
-////        }
-//////    }
-////
-////    return
-//
-//    val client = FennecGrpcClient("127.0.0.1", 64733)
-//
-//    log.atInfo().log("Connecting...")
-//    client.connect()
-//    log.atInfo().log("Connected.")
-//
-//    val rateLimiter = RateLimiter.create(1.0)
-//    var i: Long = 0
-//    while (true) {
-//        val value = Math.sin(i * 0.01) * 100
-//        log.atInfo().log("Insert $value...")
-//        val data = HashMultimap.create<String, FData>()
-//        //Random().nextInt((500 + 1) - 10) + 10 * 1.0
-//        data.put("x", FData(value, Instant.now().toEpochMilli()))
-//        var succeeded = false
-//        while (!succeeded) {
-//            try {
-//                rateLimiter.acquire()
-//
-//                client.insert(data, ns)
-//                succeeded = true
-//            } catch (e: StatusRuntimeException) {
-//                log.atWarning().log("Insert failed")
-//            }
-//        }
-//        log.atInfo().log("Inserted.")
-//        i += 5
-//    }
-////    data.put("x", FData(3.0, 1544912246000))
-////    data.put("x", FData(4.0, 1544912248000))
-////    data.put("x", FData(5.0, 1544912251000))
-//
-//
-//    val start = System.nanoTime()
-//    val query: FQuery = FSelection("x", ns, InRange(0L, 1544912246000L)).toQuery()
-//    val result = client.query(query)
-//    val end = (System.nanoTime() - start) / 1000000.0
-//    println("end:$end")
-//    println(query)
-//    println(result)
 }
